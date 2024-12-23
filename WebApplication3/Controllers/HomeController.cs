f482de44598164789bc8697888c1e36612fddeb7
long trần <pelong16042004@gmail.com>
2024-12-23 09:50:59 +0700

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Resources;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ResourceManager _resourceManager;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+            // Khởi tạo ResourceManager, chỉ định đường dẫn tệp tài nguyên
+            _resourceManager = new ResourceManager("WebApplication3.Resources.HomeController", typeof(HomeController).Assembly);
+        }
+
+        public IActionResult Index()
+        {
+            var culture = HttpContext.Features.Get<Microsoft.AspNetCore.Localization.IRequestCultureFeature>()?.RequestCulture.Culture
+                 ?? CultureInfo.CurrentUICulture;
+
+            ViewData["WelcomeMessage"] = _resourceManager.GetString("WelcomeMessage", culture);
+            ViewData["HomePageDescription"] = _resourceManager.GetString("HomePageDescription", culture);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            // Tương tự, lấy văn hóa hiện tại và chuỗi PrivacyMessage
+            var culture = HttpContext.Features.Get<Microsoft.AspNetCore.Localization.IRequestCultureFeature>()?.RequestCulture.Culture
+                          ?? CultureInfo.CurrentUICulture;
+
+            ViewData["PrivacyMessage"] = _resourceManager.GetString("PrivacyMessage", culture);
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Localization;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Đăng ký dịch vụ Localization và View
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization()
+    .AddDataAnnotationsLocalization();
+
+// Danh sách các ngôn ngữ được hỗ trợ
+var supportedCultures = new[]
+{
+    new CultureInfo("en"), // Tiếng Anh
+    new CultureInfo("vi"), // Tiếng Việt
+    new CultureInfo("fr")  // Tiếng Pháp
+};
+
+// Cấu hình Localization
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en"), // Ngôn ngữ mặc định
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+};
+
+// Thêm QueryStringRequestCultureProvider vào RequestCultureProviders
+localizationOptions.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+
+var app = builder.Build();
+
+// Middleware Localization
+app.UseRequestLocalization(localizationOptions);
+
+// Middleware cơ bản
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+// Định tuyến mặc định
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
